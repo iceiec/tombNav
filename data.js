927729b4f6ca9252c3f07cff5b188ed8ee080802
iceiec <143006091+iceiec@users.noreply.github.com>
2025-06-12 20:01:10 +0800

    Add files via upload

---
+++ data.js
@@ -0,0 +1,135 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-img_20250528_113517_00_469",
+      "name": "IMG_20250528_113517_00_469",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2992,
+      "initialViewParameters": {
+        "yaw": -0.24801204437027913,
+        "pitch": 0.3508046702233152,
+        "fov": 1.4309781496564433
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": -1.6595930940188843,
+          "pitch": 0.32411440293468985,
+          "title": "Florencia P. Cruz",
+          "text": "Florencia P. Cruz<br>"
+        },
+        {
+          "yaw": -1.5859514946549336,
+          "pitch": 0.33781815227991174,
+          "title": "Abraham M. Cruz",
+          "text": "Abraham M. Cruz"
+        },
+        {
+          "yaw": -1.5006992022932213,
+          "pitch": 0.35013125249669663,
+          "title": "Pedro Cruz",
+          "text": "Pedro Cruz"
+        }
+      ]
+    },
+    {
+      "id": "1-img_20250528_113548_00_471",
+      "name": "IMG_20250528_113548_00_471",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2992,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-img_20250528_113619_00_473",
+      "name": "IMG_20250528_113619_00_473",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2992,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
